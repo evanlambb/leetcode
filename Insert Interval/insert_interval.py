9447ac21dd93a7dcd961383670ff93f3291da56c
Evan Lamb <evanlamb848@gmail.com>
2025-08-10 10:28:40 -0400

    completed

--- Insert Interval/insert_interval.py
+++ Insert Interval/insert_interval.py
@@ -1,40 +1,78 @@
 class Solution:
-    def mergeForward(self, start, endSF, intervals, index, newLst):
-        length = len(intervals)
-        end = True
-        while index < len(intervals):
-            if endSF >= intervals[index][0] and intervals[index][1] > endSF:
-                # the chain continues!
-                endSF = intervals[index][1]
-            elif endSF < intervals[index][0] and end:
-                # we have found the end of the chain...
-                newLst.append([start, endSF])
-                end = False
-            else: # the chain has ended...
-                newLst.append(intervals[index])
-            index += 1
+    def overlap(self, i1, i2):
+        # returns true if i1 and i2 overlap, and false otherwise
+        if i1[0] < i2[0]:
+            # i1 comes first
+            if i1[1] >= i2[0]:
+                return True
 
-        return newLst
+        else:
+            #i2 comes first
+            if i2[1] >= i1[0]:
+                return True
 
+        return False
     def insert(self, intervals, newInterval):
+                # 1) insert with no merge
         newLst = []
-       
-        if newInterval[1] < intervals[0][1]:
-            newLst.append(newInterval)
+        app = False
+        inserted = False
+ 
         for i in range(len(intervals)):
-            if newInterval[0] >= intervals[i][0] and newInterval[0] <= intervals[i][1]:
-                # We have found the beginning of the chain
-                return self.mergeForward(intervals[i][0], newInterval[1], intervals, i, newLst)
-            elif i < len(intervals) - 1 and intervals[i][1] < newInterval[0] and newInterval[1] >= intervals[i][1]: #insert in between
-                return self.mergeForward(newInterval[0], newInterval[1], intervals, i, newLst)
+            if not inserted:
+                # we pick the start to be the smaller of the two
+                start = min(newInterval[0], intervals[i][0])
+                print(f"starting with {start}")
+                if self.overlap(newInterval, intervals[i]):
+                    # if we have overlap, we take the end to be the bigger one... 
+                    print("overlap!")
+                    end = max(newInterval[1], intervals[i][1])
+                    print(f"ending at {end} for now")
+                    # but now we need to look even further... 
+                    # if end == newInterval[1] I might need to keep looking further... 
+                    if end == newInterval[1]:
+                        print("need to look forward futher just to make sure...")
+                        found_end = False
+                        while i < len(intervals) - 1:
+                            if found_end:
+                                newLst.append(intervals[i])
+                            elif self.overlap(intervals[i + 1], [start, end]):
+                                print("there is overlap with the next element")
+                                end = max(intervals[i + 1][1], end)
+                                # there are more cases here
+                            else:
+                                # there was no further overlap
+                                newLst.append([start, end])
+                                #newLst.append(intervals[i])
+                                found_end = True
+                            i += 1
+                        # are we ending with the fancy new one?
+                        if not found_end:
+                            newLst.append([start, end])
+                        else:
+                            newLst.append(intervals[i])
+                        return newLst
+                        # do we need to add the last one manually, or is it a part of the merge?
+                else:
+                    # there must not have been any overlap...
+                    end = min(intervals[i][1], newInterval[1])
+
+                    # do I need to now append interval[i]?
+                    if start != intervals[i][0]:
+                        app = True
+                
+                if start <= newInterval[0] and end >= newInterval[1]:
+                    # we check if its the new interval, so we can set inserted to true
+                    inserted = True
+
+                newLst.append([start, end])
+
+                if app:
+                    newLst.append(intervals[i])
             else:
                 newLst.append(intervals[i])
 
-        # insert at the end case.
-        newLst.append(newInterval)
+        if not inserted:
+            newLst.append(newInterval)
         return newLst
-    
-
-if __name__ == "__main__":
-    sol = Solution()
-    print(sol.insert([[1,3],[6,9]], [2,5]))
+        
