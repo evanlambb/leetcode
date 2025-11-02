820706d28d742802ac0ef98bce34364db81a84f6
Evan Lamb <evanlamb848@gmail.com>
2025-11-02 18:29:38 -0500

    python

---
+++ 0155. Min Stack/min_stack.py
@@ -0,0 +1,31 @@
+class MinStack:
+
+    def __init__(self):
+        self.main_stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.main_stack.append(val)
+
+        if not self.min_stack or self.min_stack[-1] >= val:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        v = self.main_stack.pop()
+        if v == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self) -> int: # how do i handle the empty case?
+        return self.main_stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
