aec78a0e709d774002d830e76ad8ae84ebef3966
Evan Lamb <152565951+evanlambb@users.noreply.github.com>
2026-05-16 16:21:06 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (40.74%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -5,19 +5,19 @@ class Solution:
             if i >= len(s):
                 return 1
             elif int(s[i]) == 0:
-                seen[(s,i)] = 0
+                seen[i] = 0
                 return 0
-            elif (s, i) in seen:
-                return seen[(s,i)]
+            elif i in seen:
+                return seen[i]
             else:
                 #print(f"here is s: {s}\n and here is i: {i}\n and here is len(s): {len(s)}\n and here is s[i] {s[i]}" ) 
                 if i + 1 < len(s) and (int(s[i]) == 1 or (int(s[i]) == 2 and int(s[i+1]) <= 6)):
                     ans = dfs(i + 1, s) + dfs(i + 2, s)
-                    seen[(s,i)] = ans
+                    seen[i] = ans
                     return ans
                 else:
                     ans = dfs(i + 1, s)
-                    seen[(s,i)] = ans
+                    seen[i] = ans
                     return ans
 
         return dfs(0, s)
