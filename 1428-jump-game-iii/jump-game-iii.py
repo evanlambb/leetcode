de87caf5e4389f7c570c0fae1f0eeeefe0d864ef
Evan Lamb <152565951+evanlambb@users.noreply.github.com>
2026-05-17 12:44:11 -0400

    Time: 7 ms (97.28%) | Memory: 38.9 MB (23.77%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        seen = set()
+
+        def dfs(i):
+            if i >= len(arr) or i < 0:
+                return False
+            if arr[i] == 0:
+                return True
+            if i in seen:
+                return False
+            seen.add(i)
+            return dfs(i + arr[i]) or dfs(i - arr[i])
+            # if i + arr[i] not in seen and i - arr[i] not in seen:
+            #     return dfs(i + arr[i]) or dfs(i - arr[i])
+            # elif i + arr[i] not in seen:
+            #     return dfs(i + arr[i])
+            # elif i - arr[i] not in seen:
+            #     return dfs(i - arr[i])
+
+
+        return dfs(start)
